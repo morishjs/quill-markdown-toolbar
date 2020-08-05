4a522861da62a518f4030876a0c24ea13c557efc
Junsuk Park <park64kr63@gmail.com>
2020-08-05 20:47:44 +0900

    # CHORE - babel-es-2015 => @babel/preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015']
+                    presets: ['@babel/preset-env']
                 }
             }
         ]
